7f7ebba547d99ce59c22e695707f376a5e33b1c4
Alexander Ziskind <alex@nuvious.com>
2016-02-10 00:39:53 -0500

    added nativeSpring function to tns-effects.android.ts

--- tns-effects.android.js
+++ tns-effects.android.js
@@ -1,8 +1,13 @@
 var common = require("./tns-effects-common");
 global.moduleMerge(common, exports);
 var ViewEffects = (function () {
-    function ViewEffects() {
+    function ViewEffects(view) {
+        this._view = view;
     }
+    ViewEffects.prototype.nativeSpring = function (animation) {
+        //this is where the native android spring animation will be implemeted 
+        return this._view.animate(animation);
+    };
     return ViewEffects;
 })();
 exports.ViewEffects = ViewEffects;
--- tns-effects.android.ts
+++ tns-effects.android.ts
@@ -1,9 +1,17 @@
 import common = require("./tns-effects-common");
+import {View} from "ui/core/view";
 
 global.moduleMerge(common, exports);
 
 export class ViewEffects {
-    constructor() {
-        
+    private _view: View;
+    
+    constructor(view: View) {
+        this._view = view;
+    }
+    
+    public nativeSpring(animation): Promise<void> {
+        //this is where the native android spring animation will be implemeted 
+        return this._view.animate(animation);
     }
 }
